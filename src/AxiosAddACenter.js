2ad67dea769282f48488c023ca5683c5e4efe107
Dassi Teichholz <116339277+DassiTeichholz@users.noreply.github.com>
2023-05-11 23:07:12 +0300

    DVE

--- src/AxiosAddACenter.js
+++ src/AxiosAddACenter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 
@@ -8,38 +8,90 @@ const AxiosAddACenter = () => {
   const centerName = location.state.centerName;
   const jsonFile = location.state.jsonFile;
   const imagesArr=location.state.imagesArr;
-  const [loading, setLoading] = useState(false);
+ 
+  useEffect(() => {
+    sendTheMallPhotos();
+  }, []);
 
-  const handleSendData = async () => {
-    setLoading(true);
+  const convertToByteArray = async (url) => {
     try {
-      const formData = new FormData();
-      formData.append('centerName', centerName);
-      formData.append('jsonFile', JSON.stringify(jsonFile));
-      imagesArr.forEach((image, index) => {
-        formData.append(`imagesArr[${index}].item`, image.item);
-        formData.append(`imagesArr[${index}].floor`, image.floor);
-        formData.append(`imagesArr[${index}].img`, image.img);
-      });
-      await axios.post('https://localhost:7227/api/nisuy_', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      });
-      // הצלחנו לשלוח את המידע לשרת
+      const response = await axios.get(url, { responseType: 'arraybuffer' });
+      const byteArray = Array.from(new Uint8Array(response.data));
+      return byteArray;
     } catch (error) {
-      // קרתה שגיאה בשליחת המידע לשרת
+      throw new Error('Error converting image to byte array');
     }
-    setLoading(false);
+  };
+
+  const sendTheMallPhotos = async () => {
+    try {
+      const theMallPhotos = await Promise.all(
+        imagesArr.map(async (item) => {
+          const byteArray = await convertToByteArray(item.img);
+          let floor1=item.floor
+          return {
+            theMallPhotoId: 0,
+            image: byteArray,
+            floor:parseInt(floor1, 10) ,
+            centerId: 0
+          };
+        })
+      );
+      // const config = {
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data'
+      //   }
+      // };
+      debugger
+      let c={theMallPhoto:theMallPhotos,centerName:centerName,jsonFile:jsonFile}
+      let anss=await axios.post('https://localhost:7227/api/nisuy_', c);
+      console.log('TheMallPhotos sent successfully!');
+    } catch (error) {
+      console.error('Error sending TheMallPhotos:', error);
+    }
+
   };
 
   return (
     <div>
-      <button onClick={handleSendData} disabled={loading}>
-        {loading ? 'שולח...' : 'שלח דגל לשרת'}
-      </button>
+      {/* Rendering component */}
     </div>
   );
+ 
 };
 
 export default AxiosAddACenter;
+
+
+// const [loading, setLoading] = useState(false);
+
+  // const handleSendData = async () => {
+  //   setLoading(true);
+  //   try {
+  //     const formData = new FormData();
+  //     formData.append('centerName', centerName);
+  //     formData.append('jsonFile', JSON.stringify(jsonFile));
+  //     imagesArr.forEach((image, index) => {
+  //       formData.append(`imagesArr[${index}].item`, image.item);
+  //       formData.append(`imagesArr[${index}].floor`, image.floor);
+  //       formData.append(`imagesArr[${index}].img`, image.img);
+  //     });
+  //     await axios.post('https://localhost:7227/api/nisuy_', formData, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data'
+  //       }
+  //     });
+  //     // הצלחנו לשלוח את המידע לשרת
+  //   } catch (error) {
+  //     // קרתה שגיאה בשליחת המידע לשרת
+  //   }
+  //   setLoading(false);
+  // };
+
+  // return (
+  //   <div>
+  //     <button onClick={handleSendData} disabled={loading}>
+  //       {loading ? 'שולח...' : 'שלח דגל לשרת'}
+  //     </button>
+  //   </div>
+  // );
